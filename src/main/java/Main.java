83ba5a109d098485b4de0935627b40c391864926
SamuelDidoszak <samueldidoszak1@gmail.com>
2022-01-17 23:24:10 +0100

    Created the design and the layout for application

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,33 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRippler;
+import com.jfoenix.controls.JFXTextField;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    double windowWidth = 1280;
+    double windowHeight = 720;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/window.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("/window.fxml"));
         primaryStage.setTitle("Audio Cutter");
-        primaryStage.setScene(new Scene(root, 1000, 550));
+
+        Scene scene = new Scene(root, windowWidth, windowHeight);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
